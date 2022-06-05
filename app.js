22249895ed50a70181df1c88807c1d47aa811b98
Yusuff Ridwan <wemzain@gmail.com>
2022-06-05 20:13:01 +0100

    Just added some css transition and style

--- app.js
+++ app.js
@@ -4,6 +4,18 @@
 // 2. organize the file by IDs
 // with the IDs we will generate some images
 // then add then to the website
+const loadingTag = document.querySelector("header p.loading")
+const nextTag = document.querySelector("a.next")
+const prevTag = document.querySelector("a.previous")
+const stepsTag = document.querySelector('footer span')
+const sliderTag = document.querySelector("div.slider")
+const footerTag = document.querySelector("footer")
+
+let currentSlide = 0
+
+let totalSlides = 0
+
+
 const apiKey = "figd_ZrfXJpor0Eq8-xiciCu_K86WZjAKLSjtjv3O_N1c"
 const apiHeaders = {
     headers: {
@@ -21,9 +33,12 @@ const loadFile = function (key) {
                 return frame.id
             })
 
+            const title = data.name
+
             return {
                 key: key, 
-                ids: ids
+                ids: ids,
+                title: title
             }
         })
 }
@@ -32,7 +47,7 @@ const loadImages = function (obj) {
     const key = obj.key
     const ids = obj.ids.join(",")
     console.log(ids)
-    return fetch("https://api.figma.com/v1/images/" + key + "?ids=" + ids, apiHeaders)
+    return fetch("https://api.figma.com/v1/images/" + key + "?ids=" + ids + "&scale=1&format=svg", apiHeaders)
         .then(response => response.json())
         .then(data => {
             return obj.ids.map(id => {
@@ -42,12 +57,16 @@ const loadImages = function (obj) {
 }
 
 const addImagesToSite = function (urls) {
-    const sectionTag = document.querySelector("section")
+    const sliderTag = document.querySelector("div.slider")
 
-    sectionTag.innerHTML = ""
+    totalSlides = urls.length
+
+    footerTag.classList.add("show")
+
+    sliderTag.innerHTML = ""
 
     urls.forEach(url => {
-        sectionTag.innerHTML = sectionTag.innerHTML + `
+        sliderTag.innerHTML = sliderTag.innerHTML + `
         <div>
             <img src="${url}">
         </div>
@@ -56,5 +75,46 @@ const addImagesToSite = function (urls) {
 }
 
 loadFile("rqOhRFLeD0eJCf8zaoxhFN")
-    .then(ids => loadImages(ids))
+    .then(file => {
+        loadingTag.innerHTML = file.title
+        document.title = file.title + " | Figure"
+
+        return file
+    })
+    .then(file => loadImages(file))
     .then(imageUrls => addImagesToSite(imageUrls))
+
+// lets set up the slider and make it work
+
+const next = () => {
+    currentSlide = currentSlide + 1
+    if(currentSlide >= totalSlides) {
+        currentSlide = totalSlides - 1
+    }
+    moveSlider()
+}
+
+const previous = () => {
+    currentSlide = currentSlide - 1
+    if(currentSlide < 0) {
+        currentSlide = 0
+    }
+    moveSlider()
+}
+
+const moveSlider = () => {
+    sliderTag.style.transform = `translate(${currentSlide * -100}vw, 0)`
+    stepsTag.innerHTML = `${currentSlide + 1} / ${totalSlides}`
+}
+
+nextTag.addEventListener("click", function (event) {
+    
+    next()
+    event.preventDefault()
+})
+
+prevTag.addEventListener("click", (event) => {
+    
+    previous()
+    event.preventDefault()
+})
